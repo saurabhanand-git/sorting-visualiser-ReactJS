06d7ba09ae99c18eb490109261d9b3a4f63d8736
james grant <grantja92@gmail.com>
2020-12-23 14:17:51 +0000

    refactors bubble sort to return if no swaps are made

--- src/components/utils/sorting.js
+++ src/components/utils/sorting.js
@@ -2,12 +2,17 @@ export const bubbleSort = (unsortedArr) => {
   const n = unsortedArr.length;
   const inputArr = [...unsortedArr];
 
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-      if (inputArr[j] > inputArr[j + 1]) {
-        [inputArr[j], inputArr[j + 1]] = [inputArr[j + 1], inputArr[j]];
+  let sorted = false;
+
+  while (!sorted) {
+    sorted = true;
+    for (let i = 0; i < n; i++) {
+      if (inputArr[i] > inputArr[i + 1]) {
+        [inputArr[i], inputArr[i + 1]] = [inputArr[i + 1], inputArr[i]];
+        sorted = false;
       }
     }
   }
+
   return inputArr;
 };
