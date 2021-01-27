55e4cd0c9e736ac73b50b85f21d335d6146f875c
1jgrant <grantja92@gmail.com>
2021-01-27 11:48:27 +0000

    adds a done animation to the sort animation list to allow resetting of color

--- src/components/utils/sorting.js
+++ src/components/utils/sorting.js
@@ -22,3 +22,31 @@ export const bubbleSort = (unsortedArr) => {
   sortObj.sortedArray = input;
   return sortObj;
 };
+
+export const bubbleSort2 = (unsortedArr) => {
+  const n = unsortedArr.length;
+  const input = [...unsortedArr];
+  let j = 0;
+  let sorted = false;
+  const sortObj = {
+    animations: [],
+  };
+  while (!sorted) {
+    sorted = true;
+    for (let i = 0; i < n - j - 1; i++) {
+      sortObj.animations.push([i, i + 1, "comp"]);
+      if (input[i] > input[i + 1]) {
+        sortObj.animations.push([i, i + 1, "swap"]);
+        sortObj.animations.push([i, i + 1, "done"]);
+        [input[i], input[i + 1]] = [input[i + 1], input[i]];
+        sorted = false;
+      } else {
+        sortObj.animations.push([i, i + 1, "done"]);
+      }
+    }
+    j++;
+  }
+
+  sortObj.sortedArray = input;
+  return sortObj;
+};
--- src/components/utils/sorting.test.js
+++ src/components/utils/sorting.test.js
@@ -1,4 +1,4 @@
-import { bubbleSort } from "./sorting";
+import { bubbleSort, bubbleSort2 } from "./sorting";
 import { generateNewArray } from "./arrays";
 
 describe("sorting functions", () => {
@@ -34,6 +34,48 @@ describe("sorting functions", () => {
         [1, 2, "comp"],
       ];
       expect(bubbleSort(unsortedArr).animations).toEqual(expAnim);
+    });
+  });
+  describe("BubbleSort2", () => {
+    it("should return a sorted array", () => {
+      const unsortedArr = generateNewArray(200, 1, 200);
+      const sorted = [...unsortedArr].sort((a, b) => a - b);
+      expect(bubbleSort2(unsortedArr).sortedArray).toEqual(sorted);
+    });
+    it("should not mutate the input array", () => {
+      const unsortedArr = generateNewArray(200, 1, 200);
+      const unsortedArrCopy = [...unsortedArr];
+      bubbleSort2(unsortedArr);
+      expect(unsortedArr).toEqual(unsortedArrCopy);
+    });
+    it("should return a list of comparison animations for an already sorted array", () => {
+      const unsortedArr = [1, 2, 3, 4];
+      const expAnim = [
+        [0, 1, "comp"],
+        [0, 1, "done"],
+        [1, 2, "comp"],
+        [1, 2, "done"],
+        [2, 3, "comp"],
+        [2, 3, "done"],
+      ];
+      expect(bubbleSort2(unsortedArr).animations).toEqual(expAnim);
+    });
+    it("should return a list of comparison and swap animations when required", () => {
+      const unsortedArr = [1, 2, 4, 3];
+      const expAnim = [
+        [0, 1, "comp"],
+        [0, 1, "done"],
+        [1, 2, "comp"],
+        [1, 2, "done"],
+        [2, 3, "comp"],
+        [2, 3, "swap"],
+        [2, 3, "done"],
+        [0, 1, "comp"],
+        [0, 1, "done"],
+        [1, 2, "comp"],
+        [1, 2, "done"],
+      ];
+      expect(bubbleSort2(unsortedArr).animations).toEqual(expAnim);
     });
   });
 });
